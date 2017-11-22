3a091f094ff1a8c26dfd86a99e3219ea1f3c8c02
Meredith Bayne <mrdthbyn@gmail.com>
2017-11-22 06:38:01 -0800

    practice grid view and environment variable for key

--- app/src/main/java/com/meredithbayne/toppopularmovies/MainActivity.java
+++ app/src/main/java/com/meredithbayne/toppopularmovies/MainActivity.java
@@ -2,6 +2,10 @@ package com.meredithbayne.toppopularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView gridview = findViewById(R.id.movies_grid);
+        gridview.setAdapter(new MovieAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(MainActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
