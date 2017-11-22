eaee619ad90476c0885c12620a44f02383f24bf8
Meredith Bayne <mrdthbyn@gmail.com>
2017-11-22 13:08:26 -0800

    create layouts and load with dummy data

--- app/src/main/java/com/meredithbayne/toppopularmovies/MainActivity.java
+++ app/src/main/java/com/meredithbayne/toppopularmovies/MainActivity.java
@@ -2,27 +2,21 @@ package com.meredithbayne.toppopularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.GridView;
-import android.widget.Toast;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GridView gridview = findViewById(R.id.movies_grid);
-        gridview.setAdapter(new MovieAdapter(this));
+        RecyclerView recyclerView = findViewById(R.id.recyclerview_movies);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(),2);
+        recyclerView.setLayoutManager(layoutManager);
 
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
-                Toast.makeText(MainActivity.this, "" + position,
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
+        MoviePosterAdapter adapter = new MoviePosterAdapter(getApplicationContext());
+        recyclerView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/meredithbayne/toppopularmovies/MoviePosterAdapter.java
@@ -0,0 +1,68 @@
+package com.meredithbayne.toppopularmovies;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by meredithbayne on 11/21/17.
+ */
+
+public class MoviePosterAdapter extends RecyclerView.Adapter<MoviePosterAdapter.ViewHolder>  {
+
+    private Context mContext;
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7
+    };
+
+    public MoviePosterAdapter(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public MoviePosterAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.movie_poster_list_item, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MoviePosterAdapter.ViewHolder holder, int position) {
+        Picasso.with(mContext).load(mThumbIds[position]).into(holder.mImageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mThumbIds == null) {
+            return 0;
+        }
+        return mThumbIds.length;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView mImageView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            mImageView = itemView.findViewById(R.id.movie_poster);
+        }
+    }
+}
+
